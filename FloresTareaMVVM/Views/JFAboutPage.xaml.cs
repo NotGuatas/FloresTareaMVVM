1ccf3de043a3b404f66ce3c9b0a7668b72da4082
NotGuatas <josephaugustoflores@gmail.com>
2025-01-02 11:33:42 -0500

    Actualización

--- FloresTareaMVVM/Views/JFAboutPage.xaml.cs
+++ FloresTareaMVVM/Views/JFAboutPage.xaml.cs
@@ -5,14 +5,5 @@ public partial class JFAboutPage : ContentPage
 	public JFAboutPage()
 	{
 		InitializeComponent();
-    }
-
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-        {
-            if (BindingContext is Models.JFAbout about)
-            {
-                // Navigate to the specified URL in the system browser.
-                await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-            }
-         }
+	}
 }
